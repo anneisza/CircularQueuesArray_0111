93f76746995c7f69d8843f648ca08072affe819b
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-12 15:15:12 +0700

    Membuat program utama dengan perulangan while untuk memilih menu

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -113,3 +113,39 @@ class Queues{
     }
 
 };
+
+int main(){
+    Queues q;
+    char ch;
+
+    while (true)
+    {
+        try
+        {
+            cout << "Menu" <<endl;
+            cout << "1. Implement insert operation" <<endl;
+            cout << "2. Implement delete operation" <<endl;
+            cout << "3. Display values" <<endl;
+            cout << "4. Exit" <<endl;
+            cout << "Enter your choice (1-4): ";
+            cin >> ch;
+            cout << endl;
+
+            switch (ch)
+            {
+            case '1':
+                q.insert();
+                break;
+            
+            default:
+                break;
+            }
+        }
+        catch(const std::exception& e)
+        {
+            std::cerr << e.what() << '\n';
+        }
+        
+    }
+    
+}
