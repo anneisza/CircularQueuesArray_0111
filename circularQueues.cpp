680936b76ee3e0313bfd5b6f8c9ff51a1f2cd09e
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-12 15:05:06 +0700

    Membuat prosedur display

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -66,8 +66,34 @@ class Queues{
                 FRONT = 0;
             else
                 FRONT = FRONT + 1;
-
         }       
+    }
+
+    void display(){
+        int FRONT_position = FRONT;
+        int REAR_position = REAR;
+
+        //Cek apakah antrian kosong
+        if (FRONT == -1)
+        {
+            cout << "Queue is empty\n";
+            return;
+        }
+
+        cout<< "\nElements in the queue are...\n";
+
+        //Jika FRONT_position <= REAR_position, iterasi dari FRONT hingga REAR
+        if (FRONT_position <= REAR_position)
+        {
+            while (FRONT_position <= REAR_position)
+            {
+                cout<< queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout<<endl;
+        }
+        
+        
     }
 
 };
