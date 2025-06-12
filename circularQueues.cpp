92fb8a06a11a45443d0306c6e64459afeeaaadff
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-12 15:10:47 +0700

    Melengkapi prosedur display

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -102,9 +102,14 @@ class Queues{
             }
 
             FRONT_position = 0;
-            
-        }
-        
+
+            while (FRONT_position <= REAR_position)
+            {
+                cout<< queue_array[FRONT_position] << " ";
+                FRONT_position++;
+            }
+            cout<<endl;
+        }   
     }
 
 };
