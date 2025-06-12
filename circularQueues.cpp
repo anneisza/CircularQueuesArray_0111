c61b2c5ceb6bf2ab6414789b99821a783d38410f
Riza Fitria Istiqomah Zaitunah <rizaanneisza@gmail.com>
2025-06-12 14:59:24 +0700

    Elemen yang dihapus di posisi terakhir

--- circularQueues.cpp
+++ circularQueues.cpp
@@ -61,6 +61,11 @@ class Queues{
         }
         else
         {
+            //Jika elemen yang dihapus berada di posisi terakhir array, kembali ke awal array
+            if (FRONT == max - 1)
+                FRONT = 0;
+            else
+                FRONT = FRONT + 1;
 
         }       
     }
